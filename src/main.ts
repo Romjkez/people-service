4d2fcec6ad7e57e7e7a9771ac591e4920e30fde1
Romjkez <meshkov.ra@ya.ru>
2019-11-22 12:28:09 +0300

    Logging interceptor in progress

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   const options = new DocumentBuilder()
     .setTitle('Сервис "Люди" | Проектирование веб-вервисов | Мешков Р.А [171-331]')
@@ -13,7 +15,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
   await app.listen(3001);
 }
 
